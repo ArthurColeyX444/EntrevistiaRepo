476c3f5e21e879788915a7d239440cc7c86c07f2
ArthurColeyX444 <jarthurcoleyx@gmail.com>
2026-02-26 10:39:27 -0500

    Error

--- EntrevistiaWEB/EntrevistiaWEB/Startup.cs
+++ EntrevistiaWEB/EntrevistiaWEB/Startup.cs
@@ -25,6 +25,22 @@ namespace EntrevistiaWEB
                 ClientId = "35229897295-tgio1666kqtmdcggm3vasshdukbr6cf7.apps.googleusercontent.com",
                 ClientSecret = "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",
                 CallbackPath = new PathString("/signin-google")
+
+
+  Mensaje =
+  Origen =
+  Seguimiento de la pila:
+  at eval(eval at<anonymous>(eval at<anonymous>(eval at<anonymous>(eval at<anonymous>(:2:4011)))), < anonymous >:1:102)
+    at iA(eval at<anonymous>(:2:4011), < anonymous >:1438:21388)
+    at V.eval[as G](eval at<anonymous>(:2:4011), < anonymous >:1438:54857)
+    at gJ(eval at<anonymous>(:2:4011), < anonymous >:1438:37544)
+    at JA(eval at<anonymous>(:2:4011), < anonymous >:1438:12971)
+    at rJ(eval at<anonymous>(:2:4011), < anonymous >:1438:9549)
+    at y0(eval at<anonymous>(:2:4011), < anonymous >:1438:36882)
+    at new V(eval at<anonymous>(:2:4011), < anonymous >:1438:27806)
+    at M(eval at<anonymous>(:2:4011), < anonymous >:1438:4234)
+    at ku(eval at<anonymous>(:2:4011), < anonymous >:1438:36980)
+
             });
         }
     }
